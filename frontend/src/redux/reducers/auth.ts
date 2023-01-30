7c9b3fb9fe46909004710d7aae82a7f4f5fe4194
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-01-30 14:38:51 -0300

    Create function Auth

--- frontend/src/redux/reducers/auth.ts
+++ frontend/src/redux/reducers/auth.ts
@@ -1,4 +1,32 @@
 import { SIGNUP_FAIL, SIGNUP_SUCCESS } from '../actions/types'
 import axios from 'axios'
+import { AnyAction } from 'redux'
 
-const initialState = {}
+const initialState = {
+    access: localStorage.getItem('access'),
+    refresh: localStorage.getItem('refresh'),
+    isAuthenticated: null,
+    user: null,
+    loading: null
+}
+
+export default function Auth (state = initialState, action: AnyAction) {
+    const { type, payload } = action
+
+    switch (type) {
+        case SIGNUP_SUCCESS:
+        case SIGNUP_FAIL:
+            localStorage.removeItem('access')
+            localStorage.removeItem('refresh')
+            return {
+                ...state,
+                access: null,
+                refresh: null,
+                isAuthenticated: false,
+                user: null
+            }
+
+        default:
+            return state
+    }
+}
