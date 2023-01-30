8779354643a7aad3dd8bda5efa08735f7127e06d
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-01-30 14:45:25 -0300

    Add pages signup, login, activate and add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,11 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Error404 } from './containers/errors/Error404'
 import { Home } from './containers/Home'
+import { Signup } from './containers/auth/Signup'
+import { Login } from './containers/auth/Login'
+import { Activate } from './containers/auth/Activate'
+
+
 
 function App() {
 
@@ -12,8 +17,16 @@ function App() {
     <Provider store={store}>
       <Router>
         <Routes>
+          {/* Error Display */}
           <Route  path="*" element={<Error404 />} />
+
           <Route  path="/" element={<Home />} />
+
+          {/* Authentication */}
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/activate/:uid/:token' element={<Activate />} />
+
         </Routes>
       </Router>
     </Provider>
