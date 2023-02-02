6d15158d9144c48fc0ef06fe865c1bed56654459
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-02-01 22:18:09 -0300

    redux action categories, reducers categories

---
+++ frontend/src/redux/actions/categories.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import {
+    GET_CATEGORIES_FAIL,
+    GET_CATEGORIES_SUCCESS
+} from './types'
+
+
+export const get_categories = () => async (dispatch: Dispatch) => {
+    const config = {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }
+
+    try {
+        const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/category/categories/`, config)
+
+        if (res.status === 200) {
+            dispatch({
+                type: GET_CATEGORIES_SUCCESS,
+                payload: res.data
+            })
+        }
+        else {
+            dispatch({
+                type: GET_CATEGORIES_FAIL
+            })
+        }
+
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: GET_CATEGORIES_FAIL          
+        })       
+        
+    }
+}
---
+++ frontend/src/redux/reducers/categories.ts
@@ -0,0 +1,29 @@
+import { AnyAction } from 'redux'
+import {
+    GET_CATEGORIES_FAIL,
+    GET_CATEGORIES_SUCCESS
+} from '../actions/types'
+
+
+const initialState = {
+    categories: null
+}
+
+export default function Categories(state = initialState, action: AnyAction) {
+    const { type, payload } = action
+
+    switch(type) {
+        case GET_CATEGORIES_SUCCESS:
+            return {
+                ...state,
+                categories: payload.categories
+            }
+        case GET_CATEGORIES_FAIL:
+            return {
+                ...state,
+                categories: null
+            }
+        default:
+            return state
+    }
+}
