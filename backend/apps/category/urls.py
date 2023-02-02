bc145f31a5afed7f16eb2887feb7528572fc9fe5
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-02-01 22:03:28 -0300

    get categories, subcategories

--- backend/apps/category/urls.py
+++ backend/apps/category/urls.py
@@ -1,2 +1,7 @@
-from django.shortcuts import render
+from django.urls import path
 
+from .views import ListCategoriesView
+
+urlpatterns = [
+    path('categories', ListCategoriesView.as_view()),
+]
