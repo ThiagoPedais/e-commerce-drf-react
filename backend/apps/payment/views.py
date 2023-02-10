ed41885207c4d5e6ad763046adfe5db1f4f31a3e
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-02-10 20:45:59 -0300

    add environments payment in settings

--- backend/apps/payment/views.py
+++ backend/apps/payment/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import permissions, status
+from apps.cart.models import Cart, CartItem
+from apps.orders.models import Order, OrderItem
+from apps.product.models import Product
+from apps.shipping.models import Shipping
+from django.core.mail import send_mail
+import braintree
 
-# Create your views here.
+gateway = braintree.BraintreeGateway(
+    braintree.Configuration(
+        environment=settings.BT_ENVIRONMENT,
+        merchant_id=settings.BT_MERCHANT_ID,
+        public_key=settings.BT_PUBLIC_KEY,
+        private_key=settings.BT_PRIVATE_KEY,
+    )
+)
+
+
