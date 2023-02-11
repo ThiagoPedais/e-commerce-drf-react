836442f180d01fb4c3ecf1003389fc209ac84408
Thiago Pedais <luizthiago20sccp@gmail.com>
2023-02-11 19:03:15 -0300

    payment views

--- backend/apps/payment/views.py
+++ backend/apps/payment/views.py
@@ -20,3 +20,297 @@ gateway = braintree.BraintreeGateway(
 )
 
 
+class GenerateTokenView(APIView):
+    def get(self, request, format=None):
+        try:
+            token = gateway.client_token.generate()
+
+            return Response(
+                {'braintree_token': token},
+                status=status.HTTP_200_OK
+            )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving braintree token'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class GetPaymentTotalView(APIView):
+    def get(self, request, format=None):
+        user = self.request.user
+
+        tax = 0.38
+
+        shipping_id = request.query_params.get('shipping_id')
+        shipping_id = str(shipping_id)
+
+        try:
+            cart = Cart.objects.get(user=user)
+
+            # review if exist items
+            if not CartItem.objects.filter(cart=cart).exists():
+                return Response(
+                    {'error': 'Need to have items in cart'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+            cart_items = CartItem.objects.filter(cart=cart)
+            for cart_item in cart_items:
+                if not Product.objects.filter(id=cart_item.product.id).exists():
+                    return Response(
+                        {'error': 'A product with Id provided does not exist'},
+                        status=status.HTTP_404_NOT_FOUND
+                    )
+                if int(cart_item.count) > int(cart_item.product.quantity):
+                    return Response(
+                        {'error': 'Not enough items in stock'},
+                        status=status.HTTP_200_OK
+                    )
+
+                total_amount = 0.0
+                total_compare_amount = 0.0
+
+                for cart_item in cart_items:
+                    total_amount += (float(cart_item.product.price) * float(cart_item.count))
+                    total_compare_amount += (float(cart_item.product.compare_price) * float(cart_item.count))
+
+                total_amount = round(total_compare_amount, 2)
+                original_price = round(total_amount, 2)
+
+                # Coupons
+
+                # tax
+                estimated_tax = round(total_amount * tax, 2)
+                total_amount += (total_amount * tax)
+
+                shipping_cost = 0.0
+                # sending is valid
+                if Shipping.objects.filter(id__iexact=shipping_id).exists():
+                    # Add shipping in total amount
+                    shipping = Shipping.objects.get(id=shipping_id)
+                    shipping_cost = shipping.price
+                    total_amount += float(shipping_cost)
+
+                total_amount = round(total_amount, 2)
+
+                return Response(
+                    {'original_price': f'{original_price:.2f}',
+                     'total_amount': f'{total_amount:.2f}',
+                     'total_compare_amount': f'{total_compare_amount:.2f}',
+                     'estimated_tax': f'{estimated_tax:.2f}',
+                     'shipping_cost': f'{shipping_cost:.2f}'},
+                    status=status.HTTP_200_OK
+                )
+        except:
+            return Response(
+                {'error': 'Something went wrong when retrieving payment total information'},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+
+class ProcessPaymentView(APIView):
+    def post(self, request, format=None):
+        user = self.request.user
+        data = self.request.data
+
+        tax = 0.38
+
+        nonce = data['nonce']
+        shipping_id = str(data['shipping_id'])
+        # Coupon name
+
+        full_name = data['full_name']
+        address_line_1 = data['address_line_1']
+        address_line_2 = data['address_line_2']
+        city = data['city']
+        state_province_region = data['state_province_region']
+        postal_zip_code = data['postal_zip_code']
+        country_region = data['country_region']
+        phone_number = data['phone_number']
+
+        # review if data shipping is valid
+        if not Shipping.objects.filter(id__iexact=shipping_id).exists():
+            return Response(
+                {'error': 'Invalid shipping options'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        cart = Cart.objects.get(user=user)
+
+        # review if user has items in the cart
+        if not CartItem.objects.filter(cart=cart).exists():
+            return Response(
+                {'error': 'Need to have items in cart'},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        # review if it has stock
+        for cart_item in cart_items:
+            if not Product.objects.filter(id=cart_item.product.id).exists():
+                return Response(
+                    {'error': 'Transaction failed, a product Id does not exist'},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+            if int(cart_item.count) > int(cart_item.product.quantity):
+                return Response(
+                    {'error': 'Not enough items in stock'},
+                    status=status.HTTP_200_OK
+                )
+
+            total_amount = 0.0
+
+            for cart_item in cart_items:
+                total_amount += (float(cart_item.product.price) * float(cart_item.count))
+
+            # Coupons
+            # =======
+
+            total_amount += (total_amount * tax)
+
+            shipping = Shipping.objects.get(id=int(shipping_id))
+
+            shipping_name = shipping.name
+            shipping_time = shipping.time_to_delivery
+            shipping_price = shipping.price
+
+            total_amount += float(shipping_price)
+            total_amount = round(total_amount, 2)
+
+            try:
+                # Create transaction with braintree
+                new_transaction = gateway.transaction.sale(
+                    {
+                        'amount': str(total_amount),
+                        'payment_method_nonce': str(nonce['nonce']),
+                        'options': {
+                            'submit_for_settlement': True
+                        }
+                    }
+                )
+            except:
+                return Response(
+                    {'error': 'Error processing the transaction'},
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
+
+            if new_transaction.is_success or new_transaction.transaction:
+                for cart_item in cart_items:
+                    update_product = Product.objects.get(id=cart_item.product.id)
+
+                    # find product quantity after buy
+                    quantity = int(update_product.quantity) - int(cart_item.count)
+
+                    # find product quantity to sale
+                    sold = int(update_product.sold) - int(cart_item.count)
+
+                    # update product
+                    Product.objects.filter(id=cart_item.product.id).update(quantity=quantity, sold=sold)
+
+                # create order
+                try:
+                    order = Order.objects.create(
+                        user=user,
+                        transaction_id=new_transaction.transaction.id,
+                        amount=total_amount,
+                        full_name=full_name,
+                        address_line_1=address_line_1,
+                        address_line_2=address_line_2,
+                        city=city,
+                        state_province_region=state_province_region,
+                        postal_zip_code=postal_zip_code,
+                        country_region=country_region,
+                        phone_number=phone_number,
+                        shipping_name=shipping_name,
+                        shipping_time=shipping_time,
+                        shipping_price=float(shipping_price)
+                    )
+                except:
+                    return Response(
+                        {'error': 'Transaction succeeded but failed to create the order'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+
+                for cart_item in cart_items:
+                    try:
+                        # get product
+                        product = Product.objects.get(id=cart_item.product.id)
+
+                        OrderItem.objects.create(
+                            product=product,
+                            order=order,
+                            name=product.name,
+                            price=cart_item.product.price,
+                            count=cart_item.count
+                        )
+                    except:
+                        return Response(
+                            {'error': 'Transaction succeeded and order created, but failed to create an order item'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                        )
+
+                try:
+                    send_mail(
+                        'Your Order Details',
+                        'Hey ' + full_name + ','
+                        + '\n\nWe received your order!'
+                        + '\n\nGive us some time to process your order and ship it out to you.'
+                        + '\n\nYou can go on your user dashboard to check the status of your order.'
+                        + '\n\nSincerely,'
+                        + '\nShop Time',
+                        'luizthiago20sccp@gmail.com',
+                        [user.email],
+                        fail_silently=False
+                    )
+                except:
+                    return Response(
+                        {'error': 'Transaction succeeded and order created, but failed to to send email'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+
+                try:
+                    # empty cart
+                    CartItem.objects.filter(cart=cart).delete()
+
+                    # update cart to have not items
+                    Cart.objects.filter(user=user).update(total_items=0)
+                except:
+                    return Response(
+                        {'error': 'Transaction succeeded and order successful, but failed to clear cart'},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                    )
+
+                return Response(
+                    {'success': 'Transaction successful and order was created'},
+                    status=status.HTTP_200_OK
+                )
+            else:
+                return Response(
+                    {'success': 'Transaction successful and order was created'},
+                    status=status.HTTP_200_OK
+                )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
